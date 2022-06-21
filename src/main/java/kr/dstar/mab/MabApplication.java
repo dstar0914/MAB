ca801863821bd72beca731a2f9fd40a3a7aa259a
dstar0914 <demon0914@naver.com>
2022-06-22 01:58:34 +0900

    Project Initialize

---
+++ src/main/java/kr/dstar/mab/MabApplication.java
@@ -0,0 +1,13 @@
+package kr.dstar.mab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/dstar/mab/MabApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.dstar.mab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
